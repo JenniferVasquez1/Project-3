0e797785e63e16f98d3accbe8b07677162b5bb02
Jennifer Harris <jennifer.v.harris@outlook.com>
2024-08-21 21:41:10 -0400

    added interative visuals to flask

--- static/js/script.js
+++ static/js/script.js
@@ -1,30 +1,43 @@
-var map = L.map('map').setView([30,-95], 4);
+var map = L.map('map').setView([33,-95], 4);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-const init = async () => {
+let markers = L.layerGroup();
+markers.addTo(map);
+
+const renderPoints = async () => {
+
+    let month = d3.select('#month').node().value;
+
+    console.log(month);
 
     const url = '/api/Air_Quality_And_Natural_Gas';
     data = await d3.json(url);
 
-    console.log(data);
+    filterData = data.filter(obj=>obj.month == month);
 
-    data.forEach(marker => {
+    markers.clearLayers();
+
+    filterData.forEach(marker => {
         let lat = marker.lat;
         let lng = marker.lng;
         let state = marker.state_name;
         let cat = marker.Category;
         let aqi = marker.AQI;
-        // let com = marker['Commercial Consumption'];
         let total = marker.total;
         let month = marker.month;
         let population = marker.population;
 
 
-        L.circleMarker([marker.lat,marker.lng])
-        .addTo(map)
+        L.circleMarker([marker.lat,marker.lng],{
+            radius: total/20000,
+            fillColor: cat == 'Good' ? 'green' : 'yellow',
+            fillOpacity: .65,
+            color: 'transparent'
+        })
+        .addTo(markers)
         .bindPopup(`
             <h4>
                 AQI: ${aqi}<br>
@@ -38,4 +51,4 @@ const init = async () => {
     });
 };
 
-init();
+renderPoints();
