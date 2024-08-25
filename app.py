7e23868665db84e4979b78ac710809338100797b
seashell-coder <sadafhakim@yahoo.com>
2024-08-25 15:56:09 -0400

    adding seasonal analysis files

--- app.py
+++ app.py
@@ -14,3 +14,9 @@ def renderData():
     df = pd.read_csv('static/Resources/combined_df.csv')
 
     return df.to_json(orient='records')
+
+
+# Route for Seasonal Comparison of AQI and NG_consumption for top 10 States
+# @app.route('/api/seasonal_aqi_natural_gas_comparison')
+# def renderSeasonalData():
+    # return render_template('seasonal_aqi_natural_gas_comparison_for_ten_states.html')
